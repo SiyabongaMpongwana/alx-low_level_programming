584e42154a2f513427788a243d055fc128d75bec
SiyabongaMpongwana <siyabongampongwana@gmail.com>
2024-06-23 06:43:49 -0700

    Betty better work

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,21 +3,35 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
-/* function generates random numbers  */
-/* prints them on screen with their negative or positve values */
+/**
+ * main - generate random numbers
+ * function generates random numbers
+ * prints them on screen with their negative or positve values
+ */
 int main(void)
 {
+/**
+ * variable to store the random generated number
+ */
 int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
+/**random number tested against zero
+ * if greater than, prints value of n and 'is positive'
+ * if less than, prints value of n and 'is negative'
+ * if equal to zero, prints value of n and 'is zero'
+ */
 if (n > 0)
 printf("%d is positive\n", n);
 else if (n < 0)
 printf("%d is negative\n", n);
 else
 printf("%d is zero\n", n);
+/**
+ * returns a zero if successful
+ */
 return (0);
 }
 
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main - prints English alphabet
+ *
+ */
+int main(void)
+{
+int i = 97;
+/**
+ * variable holding int value of ASCII character
+ * initialized to 97 - the int value for lowercase ASCII character 'a'
+ */
+while (i <= 122)
+{
+putchar(i);
+++i; 
+}
+/**
+ * checks variable i against 122(the last lowercase character of alphabet)
+ * prints each character corresponding to changing i value
+ * loop terminates when i equals 122 - the int value for lowercase 'z' in ascii
+ */
+i = 65;
+/** new value for i - is ASCII for uppercase 'A'
+ */
+while (i <= 90)
+{
+putchar(i);
+++i;
+}
+/**
+ * i compared against 90 - uppercase 'Z' in ASCII 
+ * prints out ASCII character corresponding to value of i
+ * increments i until i equals 90 then loop terminates
+ * after printing ASCII character for 90
+ */
+putchar(10);
+/**
+ * prints newline after the loop has terminated
+ */
+return (0);
+/**
+ * return value: is void 
+ */
+}
