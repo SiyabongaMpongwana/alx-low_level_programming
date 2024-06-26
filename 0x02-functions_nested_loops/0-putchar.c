c588ef07aca90c467f093443007d310ddf1cc3f0
SiyabongaMpongwana <siyabongampongwana@gmail.com>
2024-06-26 15:13:09 -0700

    late night fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,38 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main - prints '_putchar' followd by new line
+ * _putchar - function
  *
- * return - returns 0 if successful
+ * Return: always 0
  */
-int main(void)
+int main(void) 
 {
-int toPrint[8] = {95, 112, 117, 116, 99, 104, 97, 114};
-/*
- * set of numbers that print '_putchar' when passed into the putchar function
- * array ordered, the order is set so it would print the string '_putchar'
- **/
-int i = 0;
-/**
- * variable that will used pull out elements from the 'toPrint' array and
- * pass them in the putchar function in the while loop below
- */
-while (i <= 7)
-/**
- * the is tested against the 7 which is the last number
- * before the '\0' in the 'toPrint' array
- */
-{
-putchar(toPrint[i]);
-/**
- * prints ASCII character corresponding to the number
- * pulled out by 'i' from the 'toPrint' array
- */
-++i;
-/**
- * increments i
- */}
-putchar(10);
-/* prints new line after the loop terminates */
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
-
