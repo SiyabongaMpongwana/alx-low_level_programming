6469c9d10eb2eb40e2207cffb513857214a76901
SiyabongaMpongwana <siyabongampongwana@gmail.com>
2024-06-27 16:24:16 -0700

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplys integer 'a' with 'b'
+ * Return: returns product of 'a' and 'b'
+ */
+
+int mul(int a, int b)
+{
+int results = a * b;
+return (results);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers 0 to 9
+ * Return: return is void
+ */
+void print_numbers(void)
+{
+int i;
+while (i <= 9)
+{
+i = 0;
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
