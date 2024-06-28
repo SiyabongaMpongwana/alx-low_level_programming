f8eaaa7d8065c6d9a075c4ade99b846a20c87290
SiyabongaMpongwana <siyabongampongwana@gmail.com>
2024-06-27 17:02:57 -0700

    2-mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * mul - multiplys integer 'a' with 'b'
+ * @a: integer taken in by function
+ * @b: integer taken in by function
  * Return: returns product of 'a' and 'b'
  */
 
 int mul(int a, int b)
 {
-int results = a * b;
-return (results);
+return (a * b);
 }
