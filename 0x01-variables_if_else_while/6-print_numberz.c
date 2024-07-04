77ff979624771bc2943730acb0de76aab1ab1175
SiyabongaMpongwana <siyabongampongwana@gmail.com>
2024-07-04 14:37:58 -0700

    still

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
 /**
  * main - prints out the 10 decimal digits
+ *
+ * Return: returns a zero (success)
  */
 int main(void)
 {
 int i = 48;
-/**
- * integer value for ASCII number '0'
- */
+
 while (i <= 57)
 {
 putchar(i);
 ++i;
 }
-/**
- * while i less than or eqaul to 57 - ASCII digit '9'
- * print ASCII value for each i
- * then increment i
- */
 putchar(10);
-/**
- * prints new line right after the loop has terminated
- */
 return (0);
 }
