e248797feefa732c0dd0a48845bf62fac1a789af
SiyabongaMpongwana <siyabongampongwana@gmail.com>
2024-07-04 14:42:07 -0700

    7-print_tebalpha

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,29 +1,18 @@
 #include <stdio.h>
 /**
  * main - prints the English alphabet backwards and in lowercase
- * starting with 'z' ending with 'a'
+ *
+ * Return: returns a zero if successful
  */
 int main(void)
 {
 int i = 122;
-/**
- * i initialized for the corresponding ASCII letter 'z'
- */
+
 while (i >= 97)
-/**
- * 97 is the integer value for lowercase ASCII 'a'
- */
 {
 putchar(i);
---i;  }
-/**
- * while i not 97 print each ASCII character corresponding to i
- * and decrement until i equals 97 print ASCII for 97 and then
- * exit loop
- */
+--i;
+}
 putchar(10);
-/**
- * prints new line after loop is terminated
- */
 return (0);
 }
