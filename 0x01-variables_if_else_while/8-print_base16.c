e5159e9bda7c4938faf88ed6be07b706c128bc33
SiyabongaMpongwana <siyabongampongwana@gmail.com>
2024-07-04 14:48:18 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,47 +2,20 @@
 /**
  * main - characters for hexidecimal numbers
  *
+ * Return: returns a zero if successful
  */
 int main(void)
 {
-int i = 48;
-/**
- * integer value for ASCII zero '0'
- */
-while (i <= 57)
-/**
- * 57 is ASCII for '9' the last hexidecimal number
- */
+int i;
+
+for (i = 48; i <= 57; ++i)
 {
 putchar(i);
-++i; }
-/**
- * prints corresponding ASCII value for i
- * increments i
- * prints last character at 57 then exits
- */
-i = 97;
-/**
- * new value corresponding to ASCII character 'a'
- */
-while (i <= 102)
-/**
- * 102 is ASCII for 'f' the last hexideciaml letter
- */
+}
+for (i = 97; i <= 102; ++i)
 {
 putchar(i);
-++i; }
-/**
- * prints hexidecimal letter for every value of i
- * increments i until it is equal to 102
- * prints hexidecimal letter for 102 then exits loop
- */
+}
 putchar(10);
-/**
- * prints new line after the loop has printed every hexcidecimal
- */
 return (0);
-/**
- * returns a zero for success
- */
 }
