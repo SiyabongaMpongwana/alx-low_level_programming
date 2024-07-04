a8f70c99cf76f304199da0b31596604fe1e57eb6
SiyabongaMpongwana <siyabongampongwana@gmail.com>
2024-07-04 14:30:16 -0700

    Still at it

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,40 +1,24 @@
 #include <stdio.h>
 /**
- *main - function prints out lowercase English alphabet except 'q' and 'e'
+ * main - function prints out lowercase English alphabet except 'q' and 'e'
+ *
+ * Return: returns zero on success
  */
 int main(void)
 {
 int i = 97;
-/**
- * i initialied to 97 because integer value for lowercase 'a' in ASCII
- */
 while (i <= 122)
 {
-/**
- * the 122 is ASCII 'z' in integer value
- * i is being compared against it
- */
 if ((i == 101) ^ (i == 113))
+{
 ++i;
-/**
- * 101 is 'e' and 113 is 'q' in ASCII
- * so since we don't want to print out these values
- * the program will skip them this is done by incrementing i
- * when whenever i hits these values
- */
+}
+else
+{
 putchar(i);
 ++i;
 }
-/**
- * the loop terminates once it reaches 122 - ASCII 'z'
- */
-putchar(10);
-/**
- * prints a new line after the loop
- */
-return (0);
-/**
- * returns a zero when successful
- */
 }
-
+putchar(10);
+return (0);
+}
