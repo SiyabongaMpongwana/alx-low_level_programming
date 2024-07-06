43db711ebdb6f184733906da815bc7edd23a437d
SiyabongaMpongwana <siyabongampongwana@gmail.com>
2024-07-06 14:45:57 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,21 +1,38 @@
-#include <stdio.h>
-
-int main()
+#include "main.h"
+/**
+ * times_table - prints nine times table up to 9 x 9
+ *
+ * Return: void
+ */
+void times_table(void)
 {
-   int a = 0;
+int a, b, c, d, e;
 
- for (a = 0; a <= 9; ++a) 
-   { 
-         for (int x =0 ; x <= 9; x++)
-         {   
-	     (x == 9 ) ? printf ("%2d\n", a * x) : // how I printed a new line for the 2nd version and used the 
-//	     this version  (...) ? <statement> : <statement> of "if else"
-//	     to achieve what was done by the putchar command outside the for loop
-             printf ("%2d, ", a * x);
-         }      
-//   putchar (10);      how I printed a new line for the version of the program 
-//   ++a;
-   }
-   return (0);
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if (c > 9)
+{
+d = c % 10;
+e = (c - d) / 10;
+_putchar(44);
+_putchar(32);
+_putchar(e + '0');
+_putchar(d + '0');
 }
-
+else
+{
+if (b != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+_putchar(c + '0');
+}
+}
+_putchar('\n');
+}
+}
