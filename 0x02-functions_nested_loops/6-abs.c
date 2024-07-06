f9069ee8f4d498fd5f361e21b1782f1a006ed7bc
SiyabongaMpongwana <siyabongampongwana@gmail.com>
2024-07-06 03:12:16 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - function computes absolute value of integer
+ * @a: absolute value of integer
+ *
+ * Return: returns an integer value
+ */
+int _abs(int a)
+{
+if (a >= 0)
+{
+a = a;
+}
+else
+{
+a *= -1;
+}
+return (a);
+}
